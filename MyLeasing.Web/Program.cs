c44f68bf9898eff4527561a6dd87b07064706a3c
Waldir Ozuna <wozuna@hotmail.com>
2020-01-09 02:28:27 +0100

    SeedDB

--- MyLeasing.Web/Program.cs
+++ MyLeasing.Web/Program.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.IO;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Microsoft.AspNetCore;
+//using Microsoft.AspNetCore.Hosting;
+//using Microsoft.Extensions.Configuration;
+//using Microsoft.Extensions.Logging;
+
+//namespace MyLeasing.Web
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            CreateWebHostBuilder(args).Build().Run();
+//        }
+
+//        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+//            WebHost.CreateDefaultBuilder(args)
+//                .UseStartup<Startup>();
+//    }
+//}
+
+
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using MyLeasing.Web.Data;
 
 namespace MyLeasing.Web
 {
@@ -14,11 +35,25 @@ namespace MyLeasing.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        private static void RunSeeding(IWebHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<SeedDb>();
+                seeder.SeedAsync().Wait();
+            }
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+        }
     }
 }
+
