fbaad31f3bff10b13ac65d148b8f0b24657d74fc
Waldir Ozuna <wozuna@hotmail.com>
2020-01-16 19:37:24 +0100

    Owner API Controller

---
+++ MyLeasing.Common/Models/EmailRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MyLeasing.Common.Models
+{
+    public class EmailRequest
+    {
+        [Required]
+        public string  Email { get; set; }
+    }
+}
